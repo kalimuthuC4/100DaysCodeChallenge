3645a24de6fc893d66a197bffca7a25ae087714c
KALI <96970686+kalimuthuC4@users.noreply.github.com>
2024-01-14 19:05:28 +0530

    Create 012-Binary-Search.java

---
+++ 012-Binary-Search.java
@@ -0,0 +1,19 @@
+// 704. Binary Search
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int h = nums.length-1;
+
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            if(target > nums[mid])
+            l = mid+1;
+            else if(target<nums[mid])
+            h = mid-1;
+            if(target == nums[mid])
+            return mid;
+        }
+        return -1;
+    }
+}
