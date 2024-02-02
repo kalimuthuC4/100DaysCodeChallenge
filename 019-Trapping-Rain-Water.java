77997f7c55a8010320f15204f302f17e2d772e47
KALI <96970686+kalimuthuC4@users.noreply.github.com>
2024-02-02 20:25:29 +0530

    Update 019-Trapping-Rain-Water.java

--- 019-Trapping-Rain-Water.java
+++ 019-Trapping-Rain-Water.java
@@ -5,7 +5,7 @@ public class Solution {
         int l = 0, r = h.length - 1, lmax = Integer.MIN_VALUE, rmax = Integer.MIN_VALUE, ans = 0;
         while (l < r) {
         lmax = Math.max(lmax, h[l]);
-        rmax = Math.max(rmax, h[r]);
+        rmax = Math.max(rmax, h[r]); 
         ans += (lmax < rmax) ? lmax - h[l++] : rmax - h[r--];
         }
         return ans;
