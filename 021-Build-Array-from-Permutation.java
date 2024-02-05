3fa12681f299a46c1078a4a2eef736e489ab5b9b
KALI <96970686+kalimuthuC4@users.noreply.github.com>
2024-02-05 19:56:49 +0530

    Create 021-Build-Array-from-Permutation.java

---
+++ 021-Build-Array-from-Permutation.java
@@ -0,0 +1,11 @@
+// 1920. Build Array from Permutation
+
+class Solution {
+    public int[] buildArray(int[] nums) {
+       int[] a = new int[nums.length];
+       for(int i=0;i<nums.length;i++){
+           a[i] = nums[nums[i]];
+       }
+       return a;
+    }
+}
