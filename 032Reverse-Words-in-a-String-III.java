9cdf71db7140a4db01270fc5791901f9c6274118
KALI <96970686+kalimuthuC4@users.noreply.github.com>
2024-02-23 22:00:53 +0530

    Create 032Reverse-Words-in-a-String-III.java

---
+++ 032Reverse-Words-in-a-String-III.java
@@ -0,0 +1,14 @@
+// 557. Reverse Words in a String III
+
+public class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.split("\\s+"); 
+        StringBuilder reversed = new StringBuilder();
+        for (String word : words) {
+            StringBuilder reversedWord = new StringBuilder(word);
+            reversedWord.reverse(); 
+            reversed.append(reversedWord).append(" "); 
+        }
+        return reversed.toString().trim();
+    }
+}
