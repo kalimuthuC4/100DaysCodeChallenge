60d46c5803a9ad539d5fc61c74974dc39bba3d6b
KALI <96970686+kalimuthuC4@users.noreply.github.com>
2024-03-13 22:07:59 +0530

    Create 042-Find-the-Pivot-Integer.java

---
+++ 042-Find-the-Pivot-Integer.java
@@ -0,0 +1,27 @@
+// 2485. Find the Pivot Integer
+
+public class Solution {
+    public int sum(int low, int high) {
+        int ans = 0;
+        for (int i = low; i <= high; i++) {
+            ans += i;
+        }
+        return ans;
+    }
+
+    public int pivotInteger(int n) {
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (sum(1, mid) == sum(mid, n)) {
+                return mid;
+            } else if (sum(1, mid) > sum(mid, n)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
