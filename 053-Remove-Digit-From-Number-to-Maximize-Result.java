4beaaaeffc1c372b3e9ba3c0d5007a0dec096d0c
KALI <96970686+kalimuthuC4@users.noreply.github.com>
2024-03-28 21:16:27 +0530

    Create 053-Remove-Digit-From-Number-to-Maximize-Result.java

---
+++ 053-Remove-Digit-From-Number-to-Maximize-Result.java
@@ -0,0 +1,20 @@
+// 2259. Remove Digit From Number to Maximize Result
+
+class Solution {
+    public String removeDigit(String number, char digit) {
+        List<String> list = new ArrayList<>();
+        for(int i=0;i<number.length();i++)
+        {
+            
+            if(number.charAt(i)==digit)
+            {
+                String str = number.substring(0,i) + number.substring(i+1, number.length());
+                list.add(str);
+            }
+            
+        }
+        Collections.sort(list);
+        System.out.print(list);
+        return list.get(list.size()-1);
+    }
+}
