c68a1fd0e11727099eda24fc6afa21c70c70cfea
KALI <96970686+kalimuthuC4@users.noreply.github.com>
2024-06-13 16:43:45 +0530

    Create 055-Minimum-Number-of-Moves-to-Seat-Everyone.java

---
+++ 055-Minimum-Number-of-Moves-to-Seat-Everyone.java
@@ -0,0 +1,14 @@
+// 2037. Minimum Number of Moves to Seat Everyone
+
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int s = 0;
+        for(int i=0;i<seats.length;i++)
+        {
+            s += Math.abs( seats[i]-students[i]);
+        }
+        return s;
+    }
+}
