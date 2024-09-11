051ad655c319c617f9e072fe9c0e3503033e7c65
KALI <96970686+kalimuthuC4@users.noreply.github.com>
2024-09-11 10:51:25 +0530

    Create 057-Maximum-Subarray.java

---
+++ 057-Maximum-Subarray.java
@@ -0,0 +1,16 @@
+// 53. Maximum Subarray
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int curr = 0;
+        int m = nums[0];
+        for(int i=0;i<nums.length;i++)
+        {
+            if(curr<0)
+            curr= 0;
+            curr+=nums[i];
+            m = Math.max(m,curr);
+        }
+        return m;
+    }
+}
